fb2809cdfdc8cf62144e0ab304d28b93fd7bee84
Enrique Guillermo Juárez López <enriquej@tec.mx>
2021-01-29 17:39:19 -0700

    v0.4

--- module1-solution/assignment1-starter-code/app.js
+++ module1-solution/assignment1-starter-code/app.js
@@ -6,8 +6,8 @@
         $scope.message = "";
         $scope.checkLunch = function(str) {
             if (str.split(",").length > 3) { $scope.message = "Too much!"; }
-            else if (str.split(",").length < 4 && str.split(",").length > 0) { $scope.message = "Enjoy!"; }
-            else { $scope.message = "Please enter data first"; }
+            else { $scope.message = "Enjoy!"; }
+            if (str === null) { $scope.message = "Please enter data first"; }
         };
     }
 })();
