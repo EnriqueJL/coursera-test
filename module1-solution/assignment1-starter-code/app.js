5b9aa86c6ed16b598c298170c6eb11bbc9af24de
Enrique Guillermo Juárez López <enriquej@tec.mx>
2021-01-29 17:35:59 -0700

    v0.3

--- module1-solution/assignment1-starter-code/app.js
+++ module1-solution/assignment1-starter-code/app.js
@@ -5,8 +5,8 @@
     function LunchCheckController($scope) {
         $scope.message = "";
         $scope.checkLunch = function(str) {
-            if (str.split(",").filter(a => a).length > 3) { $scope.message = "Too much!"; }
-            else if (str.split(",").filter(a => a).length < 4) { $scope.message = "Enjoy!"; }
+            if (str.split(",").length > 3) { $scope.message = "Too much!"; }
+            else if (str.split(",").length < 4 && str.split(",").length > 0) { $scope.message = "Enjoy!"; }
             else { $scope.message = "Please enter data first"; }
         };
     }
