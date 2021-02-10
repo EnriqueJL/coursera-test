9525f63c0e4f2c84aac6aac9bc2c095c8671fa08
Enrique Guillermo Juárez López <enriquej@tec.mx>
2021-02-10 16:37:52 -0700

    assignment2

---
+++ module2-solution/assignment2-starter-code/app.js
@@ -0,0 +1,54 @@
+(function() {
+    'use strict';
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+		.controller('AlreadyBoughtController', AlreadyBoughtController)
+		.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    
+	ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+    function ToBuyController(ShoppingListCheckOffService) {
+        this.items = ShoppingListCheckOffService.getToBuyItems();
+        this.checkOut = function(i) { ShoppingListCheckOffService.checkOut(i); };
+    }
+
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        this.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+    }
+
+    function ShoppingListCheckOffService() {
+        var ToBuyArray = [{
+                name: "oranges",
+                quantity: "10"
+            },
+			{
+                name: "apples",
+                quantity: "7"
+            },
+			{
+                name: "bananas",
+                quantity: "5"
+            },
+			{
+                name: "pears",
+                quantity: "3"
+            },
+            {
+                name: "watermelon",
+                quantity: "1"
+            }
+        ];
+        var AlreadyBoughtArray = [];
+        this.getToBuyItems = function() {
+            return ToBuyArray;
+        };
+        this.getAlreadyBoughtItems = function() {
+            return AlreadyBoughtArray;
+        };
+		this.CheckOff = function(i) {
+            AlreadyBoughtArray.push(ToBuyArray[i]);
+            ToBuyArray.splice(i, 1);
+        };
+    }
+})();
